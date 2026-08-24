e64320af2e3a7efe6e1d400f4f517e063d8684c0
azrisnacktsk <qobul2303@gmail.com>
2026-08-24 07:07:01 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,36 @@
-const CACHE_NAME = 'azri-snack-v1';
+const CACHE_NAME = 'azri-snack-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './admin.html',
+  './karyawan.html',
+  './manifest.json',
+  './manifest-karyawan.json',
+  './logo.png'
+];
 
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
